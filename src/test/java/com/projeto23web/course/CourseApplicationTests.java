5273e12930beb4f745b949e5082dd51e3b7faf51
Filipe Ferreira <filipe.ferreira@ibm.com>
2023-01-09 10:37:04 -0300

    Project created

---
+++ src/test/java/com/projeto23web/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto23web.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
