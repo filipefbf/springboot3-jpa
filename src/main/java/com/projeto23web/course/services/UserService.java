f0bb136807fdeea6daf0a019e17047b37f9a1bd9
Filipe Ferreira <filipe.ferreira@ibm.com>
2023-01-19 22:13:29 -0300

    Service layer, component registration

---
+++ src/main/java/com/projeto23web/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.projeto23web.course.services;
+
+import com.projeto23web.course.entities.User;
+import com.projeto23web.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
